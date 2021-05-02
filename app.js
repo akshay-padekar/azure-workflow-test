95cea6fd54a06e3b0cb6d11012b2ff2d1e270129
Akshay Padekar <akshay.padekar@gmail.com>
2021-05-03 01:42:20 +0530

    removed port number

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('./db/mongoose')
@@ -10,6 +9,6 @@ app.use(bodyParser.json());
 
 require('./routes/index')(app);
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+app.listen(() => {
+  console.log(`Server is running`);
 });
